8ca4aefde192eb885fa324b0aa21bf00eea39ce0
Joy Chowdhury <147070186+JoyChowdhury12@users.noreply.github.com>
2024-03-30 21:25:13 +0600

    First commit

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:super_meals/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  final Category category;
+
+  const CategoryGridItem({super.key, required this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).secondaryHeaderColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(colors: [
+            category.color.withOpacity(0.55),
+            category.color.withOpacity(0.9)
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context)
+              .textTheme
+              .titleLarge!
+              .copyWith(color: Theme.of(context).colorScheme.onBackground),
+        ),
+      ),
+    );
+  }
+}
